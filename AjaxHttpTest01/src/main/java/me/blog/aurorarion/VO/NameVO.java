e22f8724c4d7a5aeab4cf5abf0cca17a82b7444a
sojourner <aurorarion@gmail.com>
2017-02-21 13:39:21 +0900

    Commit for HtmlforAjaxTest

---
+++ AjaxHttpTest01/src/main/java/me/blog/aurorarion/VO/NameVO.java
@@ -0,0 +1,18 @@
+package me.blog.aurorarion.VO;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Data
+@Setter
+@Getter
+@NoArgsConstructor
+@ToString
+public class NameVO {
+
+	private String name;
+	
+}
